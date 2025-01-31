5b5c6d29a25e9c425a33e87a10291f0ae2a9d6bb
Mats Bovin <mats.bovin@gmail.com>
2025-01-31 10:41:24 +0100

    Add users

--- analytics-db/initdb.d/init.sql
+++ analytics-db/initdb.d/init.sql
@@ -9,3 +9,15 @@ CREATE TABLE downloads (
     user_agent VARCHAR(255)
 );
 CREATE INDEX created_idx ON downloads (created);
+
+CREATE TABLE sbdi_user (
+    id INTEGER NOT NULL PRIMARY KEY,
+    user_key VARCHAR(255) NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    last_updated TIMESTAMP,
+    last_login TIMESTAMP,
+    country VARCHAR(255),
+    organisation VARCHAR(255)
+);
+CREATE INDEX date_created_idx ON sbdi_user (date_created);
+CREATE INDEX last_login_idx ON sbdi_user (last_login);
--- collector.py
+++ collector.py
@@ -19,6 +19,14 @@ logger_config = {
     'password': os.getenv('LOGGER_PASSWORD'),
 }
 
+user_config = {
+    'host': os.getenv('CAS_HOST'),
+    'port': os.getenv('CAS_PORT'),
+    'database': os.getenv('CAS_DATABASE'),
+    'user': os.getenv('CAS_USER'),
+    'password': os.getenv('CAS_PASSWORD'),
+}
+
 
 def fetch_downloads_from_logger():
     downloads_query = """
@@ -97,11 +105,67 @@ def insert_downloads_into_analytics_db(downloads):
     cursor.close()
     connection.close()
 
+
+def fetch_users():
+    query = """
+select u.userid as id, sha2(username, 256) as user_key, date_created, last_updated, last_login, pc.value as country, po.value as organisation
+from users u
+left join profiles pc on u.userid = pc.userid and pc.property = 'country'
+left join profiles po on u.userid = po.userid and po.property = 'organisation'
+where last_login is not null;
+"""
+
+    connection = mysql.connector.connect(**user_config)
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return results
+
+
+def insert_users(users):
+
+    connection = psycopg2.connect(**analytics_config)
+    cursor = connection.cursor()
+
+    cursor.execute("TRUNCATE TABLE sbdi_user;")
+
+    insert_query = """
+    INSERT INTO sbdi_user (id, user_key, date_created, last_updated, last_login, country, organisation)
+    VALUES %s
+    """
+    values = [
+        (
+            row['id'],
+            row['user_key'],
+            row['date_created'],
+            row['last_updated'],
+            row['last_login'],
+            row['country'],
+            row['organisation']
+        )
+        for row in users
+    ]
+    psycopg2.extras.execute_values(cursor, insert_query, values)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+   
 if __name__ == "__main__":
     print("Fetching downloads from Logger...")
-    downloads = fetch_downloads_from_logger()
-    print(f"Fetched {len(downloads)} rows")
+    users = fetch_downloads_from_logger()
+    print(f"Fetched {len(users)} rows")
 
     print("Inserting downloads into Analytics-DB...")
-    insert_downloads_into_analytics_db(downloads)
+    insert_downloads_into_analytics_db(users)
+    print("Done")
+
+    print("Fetching users")
+    users = fetch_users()
+    print(f"Fetched {len(users)} rows")
+
+    print("Inserting users...")
+    insert_users(users)
     print("Done")
